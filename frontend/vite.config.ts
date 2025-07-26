de1160be57bba139aba67cc7e9765739782f8c39
MohamedMouloudj <mouloudy656565@gmail.com>
2025-07-26 13:15:46 +0100

    fix: updates for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,15 @@ export default defineConfig({
     },
   },
   plugins: [react(), tailwindcss()],
+  base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
